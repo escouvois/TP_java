17f52aa2bf779834d03dae3ec765abf49c623b8d
sowibrahima <ibropers@gmail.com>
2017-11-15 15:47:35 +0100

    Update IterateurListe.java

--- src/tp/v2/IterateurListe.java
+++ src/tp/v2/IterateurListe.java
@@ -4,15 +4,21 @@ import java.util.Iterator;
 
 public class IterateurListe<E> implements Iterator<E> {
 
+	/**
+	 * Méthode pour déterminer si la liste possède un prochain élément
+	 * @return boolean : Un boolean pour déterminer si la liste possède un prochain élément
+	 */
 	@Override
 	public boolean hasNext() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+	 * Méthode pour renvoyer le prochain élément de la liste
+	 * @return E : Prochain élément de la liste
+	 */
 	@Override
 	public E next() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
