0acd859dfa47c07f8e9b04a24dbf8fd0ea25508a
LucasTaillebourg <lucas.taillebourg@gmail.com>
2017-11-20 17:40:51 +0100

    Nouvelle File

--- src/tp/v5/File.java
+++ src/tp/v5/File.java
@@ -3,17 +3,6 @@ package tp.v5;
 public interface File<K extends File<K,E>, E> extends Mesurable, Iterable<E>{
 
 	/*
-	 * Accesseurs
-	 */
-	E premier(); // Premier de la file
-	K suivants(); // Ses suivants
-	default boolean estVide() {
-		return this.taille() == 0;
-	}
-	
-	int taille();
-	
-	/*
 	 * Fabriques
 	 */
 	K creer(); // fabrique d'une file vide
